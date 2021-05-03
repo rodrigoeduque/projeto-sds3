67af1cf8f6c61a313dfae2780f2cc08665289a54
Rodrigo Eustáquio Duque <32581219+rodrigoeduque@users.noreply.github.com>
2021-05-03 15:18:48 -0300

    Basic static components

---
+++ frontend/src/components/Footer/index.tsx
@@ -0,0 +1,15 @@
+import NavBar from "components/NavBar";
+
+function Footer() {
+    return (
+        <footer className="footer mt-auto py-3 bg-dark">
+            <div className="container">
+                <p className="text-light">App desenvolvido por <a href="https://github.com/rodrigoeduque" target="_blank" rel="noreferrer">Rodrigo Eustáquio Duque</a></p>
+                <p className="text-light"><small><strong>Semana Spring React</strong><br />
+      Evento promovido pela escola DevSuperior: <a href="https://instagram.com/devsuperior.ig" target="_blank" rel="noreferrer">@devsuperior.ig</a></small></p>
+            </div>
+        </footer>
+    );
+}
+
+export default Footer;
